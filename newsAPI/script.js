16ab16c11ef227bf25758efa7e66c24399948370
Ara <ayaghsizian@gmail.com>
2017-07-25 19:00:41 -0400

    working on source id

--- newsAPI/script.js
+++ newsAPI/script.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
-    var url = "https://newsapi.org/v1/sources";
+    var url = "https://newsapi.org/v1/";
     var data = {language:"en", country:"us"};
     $.ajax({
-        url: url,
+        url: url + "sources",
         data: data,
         type: "GET",
         success: function(response) {
-            // console.log(response);
-            // console.log(response.sources[2]);
+
             var sources = response.sources;
-            // console.log(sources[2]);
-            var html = "<select class='form-control' id='source'>";
+            var html = "<select class='form-control' id='sourc'>";
             $.each(sources, function(index, source){
-                // console.log(source);
                 html += "<option>" + source.name + "</option>"
             })
             html += "</select>";
             console.log(html)
             $(".form-group").html(html)
-            
-        }
-    })
+        }//success
+    })//ajax
+    var sourceSelected = $('#sourc option:selected').text();
+
+    var newsApiKey = "d9c0cf6871d04a67bb3050ac1f2091eb"
+    var articleUrl = "https://newsapi.org/v1/articles?source="+sourceSelected+"&sortBy=latest&apiKey=" + newsApiKey;
+
+    $('#sub').click(function(event){
+      event.preventDefault();
+      console.log("The button was clicked.");
+      $.ajax({
+          url: articleUrl,
+          type: 'GET',
+          success: function(res){
+            console.log("Success: res: ", res);
+            console.log("this is sourceSelected: ",sourceSelected);
+          }//success function
+      });//ajax
+
+    });
+
 })
