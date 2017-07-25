2f462e3e10236b5449e2b34b20b41e2783f12a2e
Ara <ayaghsizian@gmail.com>
2017-07-25 19:01:43 -0400

    working on val

--- newsAPI/script.js
+++ newsAPI/script.js
@@ -10,17 +10,16 @@ $(document).ready(function() {
             var sources = response.sources;
             var html = "<select class='form-control' id='sourc'>";
             $.each(sources, function(index, source){
-                html += "<option>" + source.name + "</option>"
+                html += "<option value='" + source.id + "'>" + source.name + "</option>"
             })
             html += "</select>";
             console.log(html)
             $(".form-group").html(html)
         }//success
     })//ajax
-    var sourceSelected = $('#sourc option:selected').text();
-
+    var sourceSelected = $('#sourc option:selected').val();
     var newsApiKey = "d9c0cf6871d04a67bb3050ac1f2091eb"
-    var articleUrl = "https://newsapi.org/v1/articles?source="+sourceSelected+"&sortBy=latest&apiKey=" + newsApiKey;
+    var articleUrl = "https://newsapi.org/v1/articles?source=" + sourceSelected + "&sortBy=latest&apiKey=" + newsApiKey;
 
     $('#sub').click(function(event){
       event.preventDefault();
